1e252bc9b7222dc1f5bd4ec1a0e6ba2c09b18cfd
Camila Queiroz <camimq85@gmail.com>
2024-06-28 15:54:03 -0300

    adiciona configuração de setup das páginas

--- Home.py
+++ Home.py
@@ -2,4 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Home')
+st.set_page_config(page_title='Home', page_icon='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTADYDWw-Wdk19SHqSlthiAilmWGH6NfW5FVg&s', layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+# st.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+
--- pages/1_1. Introdução.py
+++ pages/1_1. Introdução.py
@@ -2,4 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.set_page_config(page_title='Introdução', page_icon='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTADYDWw-Wdk19SHqSlthiAilmWGH6NfW5FVg&s', layout="centered", initial_sidebar_state="auto", menu_items=None)
+
 st.title('1. Introdução')
