616927ecd692fd4f2c0e0772acd2cf1a03ccafc7
Camila Queiroz <camimq85@gmail.com>
2024-06-28 16:47:44 -0300

    atualiza páginas do projeto

--- pages/2_2. Descrição do Problema.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-st.set_page_config(page_title='Descrição do Problema', page_icon='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTADYDWw-Wdk19SHqSlthiAilmWGH6NfW5FVg&s', layout="centered", initial_sidebar_state="auto", menu_items=None)
-
-st.title('2. Descrição do Problema')
